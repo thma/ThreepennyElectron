b009512efdc58f85f0a6ae83a38c717a0fca838d
Thomas Mahler <thma32@web.de>
2019-09-29 16:58:38 +0200

    clean up a bit

--- app/Ui.hs
+++ app/Ui.hs
@@ -11,7 +11,7 @@ import           Graphics.UI.Threepenny      hiding (map, start)
 import qualified Graphics.UI.Threepenny      as UI
 import           Graphics.UI.Threepenny.Core (defaultConfig, startGUI)
 
-import           Calc
+import           Calc                        (State, populate, display )
 import           Paths                       (getStaticDir)
 
 -- | main entry point from electron.js launch script
@@ -34,34 +34,37 @@ up = do
 setup :: Window -> UI ()
 setup win =
   void $ do
+    -- define page
     return win # set title "3PennyCalc"
     UI.addStyleSheet win "semantic.css"
-    out <- UI.input # set (attr "readonly") "true" 
+    -- define UI controls
+    outputBox <- UI.input # set (attr "readonly") "true" 
                     # set (attr "style") "text-align: right; min-width: 320px" 
                     # set value "0"
-    buttons <- mapM (mapM mkButton) buttonLabels
+    buttons   <- mapM (mapM mkButton) buttonLabels
 
+    -- define page DOM with 3penny html combinators
     UI.getBody win # set (attr "style") "overflow: hidden" #+
        [ UI.div #. "ui raised very padded text container segment" #+
-          [ UI.div #. "ui input focus" #+ [element out]
+          [ UI.div #. "ui input focus" #+ [element outputBox]
           , UI.table #+
               map (UI.row . map element) buttons
           ]
        ]
        
-    let clicks = buttonClicks (zip (concat buttons) (concat buttonLabels))
+    -- define event handling for button clicks
+    let clicks   = buttonClicks (zip (concat buttons) (concat buttonLabels))
         commands = fmap populate clicks
     calcBehaviour <- accumB (def :: State) commands
-    let outText = fmap display calcBehaviour
-    element out # sink value outText
+    let outText  = fmap display calcBehaviour
+    element outputBox # sink value outText
   where
     mkButton s =
       UI.button #. "ui button" # set text s # set value s # set (attr "type") "button" #
       set (attr "style") "min-width: 60px"
     buttonClicks :: [(Element, String)] -> Event String
     buttonClicks = foldr1 (UI.unionWith const) . map makeClick
-      where
-        makeClick (e, s) = UI.pure s <@ UI.click e
+      where makeClick (e, s) = UI.pure s <@ UI.click e
     buttonLabels :: [[String]]
     buttonLabels = 
       [
@@ -70,7 +73,6 @@ setup win =
       , ["1", "2", "3", "*",  "/"]
       , [".", "0", "="]
       ]
-      -- map words $ lines "7 8 9 CE C\n4 5 6 + -\n1 2 3 * /\n . 0 ="
 
 
 -- | convenience function that opens the 3penny UI in the default web browser
